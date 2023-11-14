5d8b86c93a0bdbe4e1fd66d18ccedc871ea7b4e7
pdiocee <p_diocee@hotmail.com>
2023-11-14 14:59:33 -0700

    added link to banner

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link'
 
 import Login from '../components/Login';
 import ShabadCard from '../components/ShabadCard';
@@ -22,7 +23,9 @@ export default function Page() {
       <main>
       {isLoggedIn ? (
           <>
-            <img src='bwj-banner.webp' alt='A Basement Wale Banner' style={{ display: 'block', margin: 'auto', maxWidth: '90%', borderRadius: '1rem', marginTop: '2rem' }}/>
+            <Link href='/'>
+              <img src='bwj-banner.webp' alt='A Basement Wale Banner' href='/' style={{ display: 'block', margin: 'auto', maxWidth: '90%', borderRadius: '1rem', marginTop: '2rem' }}/>
+            </Link>
             <Typography variant="h4" sx={{ padding: '1rem', textAlign: 'center', }}>Welcome to the Basement&nbsp;Wale Database</Typography>
             <Typography variant="body1" sx={{ padding: '0 1rem 1rem 1rem', textAlign: 'center' }}>Click on a shabad card and press&nbsp;play&nbsp;to&nbsp;listen!</Typography>
             <Container sx={{ paddingBottom: '15rem' }}>
